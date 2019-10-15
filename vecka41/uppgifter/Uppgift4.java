acd613ba6ab6f68c706ddc9867a8217c266407a4
mjyyy1 <malte.erlandsson@elev.ga.ntig.se>
2019-10-15 08:50:11 +0200

    m

--- vecka41/uppgifter/Uppgift4.java
+++ vecka41/uppgifter/Uppgift4.java
@@ -1,10 +1,30 @@
 package uppgifter;
 
+import java.util.Scanner;
+
 public class Uppgift4 {
 
 	public static void main(String[] args) {
-
-
+		
+		Scanner input = new Scanner(System.in);
+		
+		int i = 0;
+		int x = 0;
+		int z = 0;
+		while(!(z >= 100000 || i == 10)) {
+			
+			x = input.nextInt();
+			z += x;
+			System.out.println(z);
+			if (x>0) {
+				i++;
+			}
+			System.out.println();
+			
+			
+		}
+		
+		
 	}
 
 }
