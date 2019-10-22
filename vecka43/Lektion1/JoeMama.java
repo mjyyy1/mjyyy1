460bb861f93d9d3c603c3ca88f14dac72435318a
mjyyy1 <malte.erlandsson@elev.ga.ntig.se>
2019-10-22 14:24:47 +0200

    m

--- vecka43/Lektion1/JoeMama.java
+++ vecka43/Lektion1/JoeMama.java
@@ -1,10 +1,23 @@
 package Lektion1;
 
+import java.util.Scanner;
+
 public class JoeMama {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("ask me who Joe is");
+		String Joe = input.next();
+		String Mama = "Who is Joe";
+		String hej = "who is joe";
+		String haj = "who is Joe";
+		String hel = "Who is joe";
+		
+		if(Joe == Mama || Joe == hej || Joe == haj || Joe == hel) {
+System.out.println("JOE MAMA");
 	}
-
+	}
 }
+
--- vecka43/Lektion1/Metoder.java
+++ vecka43/Lektion1/Metoder.java
@@ -5,23 +5,47 @@ import java.util.Scanner;
 public class Metoder {
 
 	public static void main(String[] args) {
+
+		TwoNumbers();
+
+	}
+/**
+ * Skriver två heltal och skickar d vidare
+ */
+	public static void TwoNumbers() {
+
 		Scanner input = new Scanner(System.in);
 		System.out.println("skriv in två heltal eller DÖ");
 		int tal1 = input.nextInt();
 		int tal2 = input.nextInt();
-		
+
 		System.out.println(minstaTal(tal1, tal2));
+
 	}
 
-	public static String minstaTal(int tal1, int tal2) {
-		
-		if (tal1 >= tal2) {
+	/**
+	 * jämför två heltal och tar reda på villket som är minst och skickar tillbaka
+	 * "metoden är klar"
+	 * 
+	 * @param tal1
+	 * @param tal2
+	 * @return String hej
+	 */
+	public static String minstaTal(int tal1, int tal2) { // om man skickar tillbaka en string ska det stå string efter
+															// static, är det en int ska det stå int
+		Scanner input = new Scanner(System.in);
+		System.out.println("skriv in två heltal eller DÖ");
+		int a = input.nextInt();
+		int b = input.nextInt();
+
+		System.out.println(minstaTal(tal1, tal2));
+		if (tal1 >= tal2) { //
 			System.out.println("minsta talet är: " + tal2);
 		} else {
 			System.out.println("minsta talet är: " + tal1);
 		}
-		String hej = "Metoden är klar";
-		
+		String hej = "Programmet är färdigt";
+
 		return hej;
 
 	}
